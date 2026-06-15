39d2cb852eca7a3cd907feba4bef3b13f541c745
unknown <romitshrestha10@gmail.com>
2026-06-15 10:05:29 +1000

    deployment started

--- src/db/db.ts
+++ src/db/db.ts
@@ -5,10 +5,11 @@ import { sharedExpense } from "../models/sharedExpenses";
 
 const connection = new Sequelize({
   dialect: "mysql",
-  host: "localhost",
-  username: "root",
-  password: "password",
-  database: "expense",
+   host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT),
   logging: false,
   models: [User, Expense, sharedExpense, Summary, Cycle],
 });
